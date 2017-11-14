bbb71f48b323cf19d505e2bf97da0c461d483541
Sam Turrell <sam.turrell@netsells.co.uk>
2017-11-14 14:31:20 +0000

    Find the input if not the selected element

--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ function plugin (Vue, options = {}) {
   // Provide a directive for formatting fields
   Vue.directive('payment', {
     bind(el, binding) {
+      // If used on a component, find the input node
+      if (el.tagName.toLowerCase() !== 'input') {
+        el = $(el).find('input')[0];
+      }
+      // An element is required
+      if (!el) return;
       // Arg is required
       if (!binding.arg) return;
       // DOM is case-insensitive, so values passed through will be lowercase, let\s find the correct equivalent
